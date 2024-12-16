2bfdd626a1f4e47b96bbcd275564870d92c9e0d4
Juozapas <juozapas.ivanauskas@mf.stud.vu.lt>
2024-12-16 10:53:44 +0200

    Adding script to compute WPS

---
+++ scripts/WPS.py
@@ -0,0 +1,28 @@
+import numpy as np
+
+bed_file_path = ""
+output_file_path = ""
+window = 120
+half_window = window // 2
+chr_length = 
+scores = np.zeros(chr_length)
+
+with open(bed_file_path, "r") as file:
+    for line in file:
+        # Skip comments or headers if necessary
+        if line.startswith("#") or line.strip() == "":
+            continue
+
+        # Split the line into columns
+        columns = line.strip().split("\t")
+        chrom, start, end = columns[:3]
+
+        length = end - start
+        if length <= window:
+            scores[start:end] -= 1
+        else:
+            scores[start:(start+half_window)] -= 1
+            scores[(start+half_window):(end-half_window)] += 1
+            scores[((start+half_window):end] -= 1
+                   
+np.savetxt(output_file_path, array, fmt="%d")
